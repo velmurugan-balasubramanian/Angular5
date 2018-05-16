1495c0d4563a957099b100f6a041ca039f7973c3
Velmurugan Balasubramanian <velmurugan1124@gmail.com>
2018-05-16 07:07:26 +0530

    Modified login form

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
+  value: Date;
+
+  update(value: Date) { 
+    console.log(value);
+    this.value = value; }
   constructor() { }
 
   ngOnInit() {
